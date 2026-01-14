3b6226fa9bd76eda5ce62da3b80ad6e3944d47a2
Jerry Zhang <jez020@ucsd.edu>
2026-01-13 19:31:46 -0800

    updated home page to display the admin page temporarily

--- frontend/src/app/admin-page/admin.tsx
+++ frontend/src/app/admin-page/admin.tsx
@@ -1,3 +1,14 @@
 /**
  * admin file
  */
+"use client";
+
+import { ThemeProvider } from "@tritonse/tse-constellation";
+
+export default function Admin() {
+  return (
+    <ThemeProvider>
+      <h1>Hello, TSE ADMIN PAGE</h1>
+    </ThemeProvider>
+  );
+}
