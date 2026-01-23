2ce84182e0ae26e07be1c7ff884035c835f842a8
Jerry Zhang <jez020@ucsd.edu>
2026-01-23 10:30:26 -0800

    updated detailbutton to match the figma more

--- frontend/src/app/admin-page/components/DetailButton.tsx
+++ frontend/src/app/admin-page/components/DetailButton.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 export type ButtonProps = {
@@ -5,15 +6,20 @@ export type ButtonProps = {
 } & React.ComponentProps<"button">;
 
 export const DetailButton = function DetailButton({
-  ref,
   mode,
-  ...props
 }: ButtonProps & { ref?: React.RefObject<HTMLButtonElement | null> }) {
-  const label = mode === "view" ? "View Details" : "Hide Details";
+  // const label = mode === "view" ? "View Details" : "Hide Details";
+  const hidden = mode !== "view"; // If not view mode, then details are hidden
   return (
-    <button ref={ref} type="button" {...props}>
-      <p>{label}</p>
-      <img src="/downCarat.svg"></img>
-    </button>
+    <div style={{ display: "flex", gap: "10px", justifyContent: "flex-end", width: "55%" }}>
+      <Image src="/downCarat.svg" width="16" height="16" alt="Down Carat"></Image>
+      <Image
+        src={hidden ? "/eyeWithSlash.svg" : "/eye.svg"}
+        width="16"
+        height="16"
+        alt={hidden ? "Hide Details" : "View Details"}
+      ></Image>
+      <Image src="/more.svg" width="16" height="16" alt="More Options"></Image>
+    </div>
   );
 };
