38e89a1d23ef599d46a89749ce309ff8e53e28ab
Jennifer Zhu <90798447+jenniferrzhu@users.noreply.github.com>
2026-01-25 17:41:35 -0800

    Integrated Top part of About us page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,47 +1,84 @@
-import React from "react";
+"use client";
+
+import { ThemeProvider } from "@tritonse/tse-constellation";
 
 import { DirectorBox } from "./components/DirectorBox";
-import styles from "./page.module.css";
+import "./styles.css";
 
-export default function ExamplePage() {
+export default function AboutUsPage() {
   return (
-    <div>
-      <div className={styles.directorBoxContainer}>
-        {/* test boxes */}
-        <DirectorBox
-          title="What We Do"
-          textBody="We empower individuals to thrive by offering clients financial literacy training, comprehensive re-entry services, job opportunities, and a path to entrepreneurship."
-          buttonlabel="Our Services"
-          imageUrl="Map_Marker.svg"
-          backgroundUrl="Our_Goal_Background.jpg"
-        />
-        <DirectorBox
-          title="Our Goal"
-          textBody="CRED's main goal is to provide case management support along with interpersonal skill training support so our clients can thrive as self-sufficient individuals."
-          buttonlabel="Support Us"
-          imageUrl="folder_people.svg"
-          backgroundUrl="Our_Goal_Background.jpg"
-        />
-      </div>
+    <ThemeProvider>
+      <div>
+        <div className="about-page">
+          <div className="about-top-section">
+            <div className="about-container">
+              <h1 className="about-title">About CRED</h1>
+              <p className="about-intro">
+                <strong>Community Realignment Education Development (CRED)</strong> was founded in
+                2018 by DeQuan Patterson, a Formerly Incarcerated Individual.
+              </p>
+            </div>
+          </div>
 
-      <div className={styles.startJourneyContainer}>
-        {/* below the why cred? */}
-        <div className={styles.textContainer}>
-          <h2>Start Your Journey With CRED</h2>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum sunt minima, iure
-            recusandae fuga ipsum quo reprehenderit impedit debitis consequuntur corporis nisi
-            sapiente suscipit placeat nemo, doloremque id, nobis voluptatem esse optio? Quidem animi
-            temporibus quisquam tempore nulla debitis vel odio nisi error, quo facilis voluptatem
-            dolores, distinctio, ex similique mollitia officiis quos facere numquam illum obcaecati
-            laboriosa.
-          </p>
+          <div className="about-vision-section">
+            <div className="about-container">
+              <div className="about-grid">
+                <div className="about-vision-wrapper">
+                  <h2 className="about-vision-heading">Our Vision</h2>
+                  <div className="about-vision-content">
+                    <p>
+                      We seek to address systemic issues that impact our communities, including
+                      racial inequality, economic disparities, and barriers to higher education and
+                      employment opportunities.
+                    </p>
+                    <p>
+                      Through education, interpersonal development of life skills and lived
+                      experience mentorship, CRED&apos;s empowerment and healing centered engagement
+                      ensures clients success.
+                    </p>
+                  </div>
+                </div>
+
+                <div className="about-image-placeholder">{/* Insert Image */}</div>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* apply to cred page link */}
-        <a className={styles.applyButton} href="">
-          Apply to CRED
-        </a>
+
+        <div className="directorBoxContainer">
+          <DirectorBox
+            title="What We Do"
+            textBody="We empower individuals to thrive by offering clients financial literacy training, comprehensive re-entry services, job opportunities, and a path to entrepreneurship."
+            buttonlabel="Our Services"
+            imageUrl="/Map_Marker.svg"
+            backgroundUrl="/Our_Goal_Background.jpg"
+          />
+          <DirectorBox
+            title="Our Goal"
+            textBody="CRED's main goal is to provide case management support along with interpersonal skill training support so our clients can thrive as self-sufficient individuals."
+            buttonlabel="Support Us"
+            imageUrl="/folder_people.svg"
+            backgroundUrl="/Our_Goal_Background.jpg"
+          />
+        </div>
+
+        <div className="startJourneyContainer">
+          <div className="textContainer">
+            <h2>Start Your Journey With CRED</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum sunt minima, iure
+              recusandae fuga ipsum quo reprehenderit impedit debitis consequuntur corporis nisi
+              sapiente suscipit placeat nemo, doloremque id, nobis voluptatem esse optio? Quidem
+              animi temporibus quisquam tempore nulla debitis vel odio nisi error, quo facilis
+              voluptatem dolores, distinctio, ex similique mollitia officiis quos facere numquam
+              illum obcaecati laboriosa.
+            </p>
+          </div>
+          <a className="applyButton" href="">
+            Apply to CRED
+          </a>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
