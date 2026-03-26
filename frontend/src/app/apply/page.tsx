5e475f1e73b85b56c478ff02f6a6bd3155def432
Jennifer Zhu <90798447+jenniferrzhu@users.noreply.github.com>
2026-03-26 12:25:59 -0700

    Apply Page Banner

--- frontend/src/app/apply/page.tsx
+++ frontend/src/app/apply/page.tsx
@@ -6,16 +6,24 @@ import { ThemeProvider } from "@tritonse/tse-constellation";
 // import NextButton from "./components/NextButton";
 // import CDCRAlert from "./components/CDCRAlert";
 
+import "./styles.css";
+
 export default function ApplyPage() {
   return (
     <ThemeProvider>
       {/* page title section */}
+      <div className="apply-top-section">
+        <div className="apply-container">
+          <h1 className="apply-title">Join Cred Today</h1>
+          <p className="apply-intro">
+            Your journey to self-sufficiency starts here. We empower you with the tools to thrive,
+            not just survive.
+          </p>
+        </div>
+      </div>
+
       <div className="page-title-banner">
-        <h1>Join Cred Today</h1>
-        <p>
-          Your journey to self-sufficiency starts here. We empower you with the tools to thrive, not
-          just survive.
-        </p>
+        <p>Insert Form</p>
       </div>
 
       <div className="form-space">{/* add form components here */}</div>
