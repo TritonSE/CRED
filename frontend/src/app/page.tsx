f8a0ef2b9f34c5d44d3fb4b2e8d55fb6494c794a
km322 <mittal.ketan1@gmail.com>
2026-06-14 22:58:44 -0700

    quick fix...

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
         unoptimized
         title="Supporting Community, Stability, and Brighter Futures"
         subtitle="CRED is a reintegration and economic development organization focused on bridging the gap between economic independence, incarceration and stability through practical support, workforce development, and mentorship."
-        subtitleEmphasis="bold"
         priority
       />
       <Mission />
